3818c5291a394b2d46fcb0b86ef8babb4cab4d29
Luiza Araújo <141930552+luizaaraujoKL@users.noreply.github.com>
2023-08-24 11:43:25 -0300

    Add files via upload

--- teste/helloword.py
+++ teste/helloword.py
@@ -6,8 +6,13 @@ peso = float(input('digite seu peso (kg):'))
 altura = float(input('digite sua altura (m):'))
 IMC = peso/(altura**2)   
 print("Seu IMC é: ", IMC)       #iimc isso é uma forma de dar uma entrada      
-print ("Muito abaixo do peso?", IMC  < 17)            
-print ("Estou abaixo do peso?", 17 <= IMC < 18.5)
-print ("Peso dentro do normal?", 18.5 <= IMC < 25.0)
-print ("Acima do peso?", 25.0 <= IMC < 30)
-print ("Muito acima do peso?", 30 <= IMC > 30)
+if IMC < 17:      
+    print ("Muito abaixo do peso", IMC)   
+elif IMC >=  17 <= IMC < 18.5:
+    print ("Estou abaixo do peso")
+elif IMC >= 18.5 <= IMC < 25.0:
+    print ("Peso dentro do normal")
+elif IMC >= 25.0 <= IMC < 30:
+    print ("Acima do peso?")
+else:
+    print ("Muito acima do peso")
